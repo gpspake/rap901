c21d8556f7824f06955534a684d95dd61d4b1be8
George Spake <george@georgespake.com>
2024-08-06 20:42:13 -0500

    test artist routes

---
+++ backend/app/tests/utils/artist.py
@@ -0,0 +1,24 @@
+import random
+
+from faker import Faker
+from sqlmodel import Session
+
+from app import crud
+from app.models.database_models import Artist
+from app.models.artist import ArtistCreate
+
+fake = Faker()
+
+
+def build_random_artist() -> ArtistCreate:
+    return ArtistCreate(
+        name=fake.word(),
+        profile=fake.sentence(nb_words=3).title().rstrip("."),
+        discogs_id=fake.random_int(min=1, max=999),
+        discogs_resource_url=fake.url(),
+    )
+
+
+def create_random_artist(db: Session) -> Artist:
+    artist_in = build_random_artist()
+    return crud.create_artist(session=db, artist_in=artist_in)
